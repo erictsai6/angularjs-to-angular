9f4f8d31ee9d4ab4178814897589fd6cc125b9bb
Eric Tsai <etsai@grubhub.com>
2019-01-06 17:21:02 -0500

    Adjusted script so that you can run multiple type conversions in one go

--- src/index.js
+++ src/index.js
@@ -1,57 +1,61 @@
 #!/usr/bin/env node
 const path = require('path');
 const glob = require('glob');
+let converted = false;
 
 const argv = require('yargs') // Reference -> https://github.com/yargs/yargs
     .option('components', {
         alias: 'c',
-        describe: 'Processes the components. Provide a glob in quotes.'
+        describe: 'Processes the components. Provide a glob in quotes.',
     })
     .option('services', {
         alias: 's',
-        describe: 'Processes the services. Provide a glob in quotes.'
+        describe: 'Processes the services. Provide a glob in quotes.',
     })
     .option('serviceSpecs', {
-        describe: 'Processes the non component spec files'
+        describe: 'Processes the non component spec files',
     })
     .option('templates', {
         alias: 't',
-        describe: 'Processes the templates. Provide a glob in quote.'
+        describe: 'Processes the templates. Provide a glob in quote.',
     })
-    .help('h')
-    .argv;
-
+    .help('h').argv;
 
 /* Process Components */
 if (argv.components) {
     const globPath = path.join(process.cwd(), argv.components);
     processComponents(globPath);
-    process.exit(0);
+    converted = true;
 }
 
 /* Process Services */
 if (argv.services) {
     const globPath = path.join(process.cwd(), argv.services);
     processServices(globPath);
-    process.exit(0);
+    converted = true;
 }
 
 /* Process Non Component Spec Files */
-if(argv.serviceSpecs) {
+if (argv.serviceSpecs) {
     const globPath = path.join(process.cwd(), argv.serviceSpecs);
     processServiceSpecs(globPath);
-    process.exit(0);
+    converted = true;
 }
 
 /* Process Templates */
 if (argv.templates) {
     const globPath = path.join(process.cwd(), argv.templates);
     processTemplates(globPath);
-    process.exit(0);
+    converted = true;
 }
 
-console.log('No arguments provided, nothing converted');
-process.exit(0);
+if (converted) {
+    console.log('Conversion completed');
+    process.exit(0);
+} else {
+    console.log('No arguments provided, nothing converted');
+    process.exit(1);
+}
 
 function processComponents(globPath) {
     const tool = require('./tools/process-components');
